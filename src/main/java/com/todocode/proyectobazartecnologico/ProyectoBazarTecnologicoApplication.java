89ba863a335169f170b91bdfa1004955ef9c55f9
jose.wexel <jose.wexel@estudiantes.utec.edu.uy>
2025-06-26 18:43:24 -0300

    Se creó el repositorio

---
+++ src/main/java/com/todocode/proyectobazartecnologico/ProyectoBazarTecnologicoApplication.java
@@ -0,0 +1,13 @@
+package com.todocode.proyectobazartecnologico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBazarTecnologicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBazarTecnologicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todocode/proyectobazartecnologico/ProyectoBazarTecnologicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.proyectobazartecnologico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBazarTecnologicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
